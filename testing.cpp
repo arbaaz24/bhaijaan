070d0c7479fc568eaa104e3b3ccd48ca36419f40
arbaaz <nasaarbaaz@gmail.com>
2020-10-26 22:27:24 +0530

    dusra created

--- testing.cpp
+++ testing.cpp
@@ -1,7 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){	
-	cout<<"Hello , World !"<<"\n";
-	cout<<"new edit";
-	cout<<"\ni made the code cleaner in this one\n";
+	cout<<"i am now in DUSRATESTING"; 
 }
